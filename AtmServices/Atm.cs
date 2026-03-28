02d0b0bdfce527c6be349538cb51cb34c7bae900
amharrawood <120509907+amharrawood@users.noreply.github.com>
2026-03-28 23:22:55 +0000

    Tests Pass

---
+++ AtmServices/Atm.cs
@@ -0,0 +1,31 @@
+﻿namespace AtmServices;
+
+public class Atm
+{
+    public int balance;
+
+    public Atm(int initialbalance) {
+        this.balance = initialbalance;
+    }
+
+    public bool withdraw(int amount) {
+        if(amount < balance) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public bool deposit (int amount) {
+        if(amount < 0) {
+            return false;
+        }
+        balance += amount;
+        return true;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
